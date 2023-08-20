f34a64ba4ec9d7e19685ab8f417bf9f024a31c1e
Maxim Shlyaptsev <118232796+MaximShly@users.noreply.github.com>
2023-08-20 17:51:23 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,5 +1,34 @@
-function Deposit(){
+function Deposit() {
+  const [amount, setAmount] = React.useState('');
+  const [success, setSuccess] = React.useState('');
+  const ctx = React.useContext(UserContext);
+
+  const user = ctx.users.find(user => user.name === 'abel');
+
+  function handleDeposit() {
+      let numAmount = Number(amount);
+      if (isNaN(numAmount) || numAmount < 0) {
+          alert("Please enter a valid positive number!");
+          return;
+      }
+      user.balance += numAmount;
+      setSuccess(`Deposited $${numAmount}. New balance: $${user.balance}`);
+      setAmount('');
+  }
+
   return (
-    <h1>Deposit</h1>
+      <Card
+          bgcolor="primary"
+          header="Deposit"
+          title={`Balance: $${user.balance}`}
+          text={success}
+          body={
+              <div>
+                  Amount<br />
+                  <input type="input" className="form-control" value={amount} onChange={e => setAmount(e.currentTarget.value)} /><br />
+                  <button type="submit" className="btn btn-light" onClick={handleDeposit}>{success ? 'Deposit Again' : 'Deposit'}</button>
+              </div>
+          }
+      />
   )
 }
